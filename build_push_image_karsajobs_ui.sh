dcd49e21948bfd2f247f6305151bb0a6b9e3c6e0
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-11-05 17:38:56 +0700

    add comments to .sh

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,7 +1,11 @@
+# build docker image bernama ahmadauni/karsajobs-ui dengan tag latest
 docker build -t ahmadauni/karsajobs-ui:latest .
 
+# mengubah nama image yang sebelumnya merujuk di local menjadi GitHub Packages
 docker tag ahmadauni/karsajobs-ui:latest ghcr.io/auni-st/ahmadauni/karsajobs-ui:latest
 
+# login ke GitHub Packages 
 echo $TOKEN_GITHUB_PACKAGES | docker login ghcr.io -u auni-st --password-stdin
 
+# mengunggah image ke GitHub Packages
 docker push ghcr.io/auni-st/ahmadauni/karsajobs-ui:latest
